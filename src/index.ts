0f371b89b3eab7347bbf4a5967893a21c31dcc07
DaffaIkhwan <12250110979@students.uin-suska.ac.id>
2025-12-15 23:06:44 +0700

    Setup Vercel entry point

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { AppModule } from './app.module';
+
+const server = express();
+
+const createNestServer = async (expressInstance) => {
+    const app = await NestFactory.create(
+        AppModule,
+        new ExpressAdapter(expressInstance),
+    );
+
+    // Enable CORS
+    app.enableCors({
+        origin: '*',
+        credentials: true,
+    });
+
+    await app.init();
+    return app;
+};
+
+// Cache the instance
+let isInitialized = false;
+
+export default async function (req, res) {
+    if (!isInitialized) {
+        await createNestServer(server);
+        isInitialized = true;
+    }
+    server(req, res);
+}
